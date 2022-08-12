cabecf8528f054ceb15b572c6d8bb23d04b46d54
metevarol <metevarol55@gmail.com>
2022-08-12 11:05:14 +0300

    BMI Calculator
    BMI Calculator with Swift, UiKit and MVC.

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Mete Varol on 12.08.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
